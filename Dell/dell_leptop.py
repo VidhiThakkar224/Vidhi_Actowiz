79b1837027e83d5dc96317107322c5e9fc05b462
VidhiThakkar224 <vidhithakkaractowiz@gmail.com>
2026-05-29 12:41:46 +0530

    Dell leptop scrape

--- Dell/dell_leptop.py
+++ Dell/dell_leptop.py
@@ -2,7 +2,6 @@ import json
 from rich import print
 from lxml import html
 from curl_cffi import requests
-import jmespath
 
 headers = {
     'accept': '*/*',
@@ -21,74 +20,74 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
 }
 
+home_url="https://www.dell.com/en-in/lp"
+response = requests.get(home_url,headers=headers,impersonate="chrome120")
+tree = html.fromstring(response.text)
 
-response=requests.get("https://www.dellstore.com/laptops.html",impersonate="chrome120")
-data=response.content.decode('utf-8')
-base_url="https://www.dellstore.com/laptops.html"
+category_links = tree.xpath("//ul[contains(@aria-label,'Computers & Accessories')]//a/@href")
+
+category_links = [
+    "https:" + link if link.startswith("//")
+    else link
+    for link in category_links
+]
+print(category_links)
+
 result=[]
 
+for category_url in category_links:
 
-for page in range(1, 8):
+    for page in range(1, 13):
 
-    page_url = f"{base_url}?p={page}"
-    response = requests.get(
-        page_url,
-        impersonate="chrome120"
-    )
+        page_url = f"{category_url}?p={page}"
+        response = requests.get(page_url,headers=headers,impersonate="chrome120")
 
-    tree = html.fromstring(response.text)
+        tree = html.fromstring(response.text)
 
-    url="https:"
+        url="https:"
 
-    #category=tree.xpath("//ul[contains(@aria-label,'Computers & Accessories')]//a")
-    products=tree.xpath("//div[@class='product details product-item-details']")
-    print(len(products))
-    images = tree.xpath("//img[contains(@class,'product-image-photo')]/@data-src")
-    # print(len(images))
-   
-    for idx,product in enumerate(products):
-        product_url=product.xpath(".//a[@class='product-item-link']/@href")
-        product_name=product.xpath(".//a[@class='product-item-link']/text()")
-        product_price=product.xpath(".//span[@class='price']/text()")[0]
-        product_images = (images[idx] if idx < len(images) else None)
-        # product_ratings=product.xpath(".//div[@class='pr-snippet-rating-decimal']/text()")
-        technical_specs = product.xpath(".//ul[@class='cf-hero-bts-list']/li")
-        technical_details = {}
+        #categories=tree.xpath("//ul[contains(@aria-label,'Computers & Accessories')]//a")
+        products=tree.xpath("//div[@class='product-item-info']")
 
-        for spec in technical_specs:
-            key = spec.xpath("normalize-space(.//div[@class='ux-module-title'])")
-            value = spec.xpath("normalize-space(.//div[@class='ux-module-content'])")
+        for product in products:
+            product_url=product.xpath(".//a[@class='product-item-link']/@href")
+            product_name=product.xpath(".//strong//a[@class='product-item-link']/text()")
+            product_price=float(product.xpath(".//span[@class='price']/text()")[0].replace("₹", "").replace(",", "").strip())
+            product_images = product.xpath(".//img/@src")
+            technical_specs = product.xpath(".//ul[@class='cf-hero-bts-list']/li")
+            technical_details = {}
 
-            if key and value:
-                technical_details[key] = value
+            for spec in technical_specs:
+                key = spec.xpath("normalize-space(.//div[@class='ux-module-title'])")
+                value = spec.xpath("normalize-space(.//div[@class='ux-module-content'])")
 
-        offer_blocks = product.xpath(".//div[@class='modalpdpdata']/div[@class='offer-title']")
+                if key and value:
+                    technical_details[key] = value
 
-        special_offers = []
+            offer_blocks = product.xpath(".//div[@class='modalpdpdata']/div[@class='offer-title']")
 
-        for offer in offer_blocks:
+            special_offers = []
 
-            offer_title = offer.xpath("normalize-space(.//a)")
+            for offer in offer_blocks:
+                offer_title = offer.xpath("normalize-space(.//a)")
+                offer_description = offer.xpath("normalize-space(following-sibling::div[@class='offer-description'][1])")
+                special_offers.append({
+                    "offer_title": offer_title,
+                    "offer_description": offer_description
+                })
 
-            offer_description = offer.xpath("normalize-space(following-sibling::div[@class='offer-description'][1])")
-
-            special_offers.append({
-                "offer_title": offer_title,
-                "offer_description": offer_description
+            order_code = product.xpath(".//div[@class = 'ps-oc']/text()")[0].split(" ")[-1]
+        
+            result.append({
+                "category_url":category_url,
+                "product_name":product_name[0].strip(),
+                "product_url":product_url[0].strip(),
+                "product_price":product_price,
+                "product_images":product_images,
+                "technical_details":technical_details,
+                "special_offers": special_offers,
+                "order_code":order_code
             })
-
-        order_code = product.xpath(".//div[@class = 'ps-oc']/text()")
-      
-        result.append({
-            "product_name":product_name[0].strip(),
-            "product_url":product_url[0].strip(),
-            "product_price":product_price,
-            # "product_images":product_images,
-            # "product_rating":product_ratings
-            "technical_details":technical_details,
-            "special_offers": special_offers,
-            "order_code":order_code
-        })
 
 print(result)
 print(len(result))
---
+++ Dell/dell_leptop_data.py
@@ -0,0 +1,19 @@
+import pymongo
+import json
+from rich import print
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["Dell_leptop"]
+mycol = mydb["leptop"]
+mycol.create_index("order_code",unique=True)
+
+with open (r"C:\Python Training\Dell\dell_scraped_data.json","r",encoding= "utf-8")as f:
+    data = json.load(f)
+
+try:
+    mycol.insert_many(data)
+    print("Data inserted")
+except Exception as e:
+    print("Dupliked product skipped")
+    print(e)
