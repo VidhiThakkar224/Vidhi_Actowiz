2072e610bb5a03799eb7418b24f17dac9d5bb354
VidhiThakkar224 <vidhithakkaractowiz@gmail.com>
2026-06-01 16:26:20 +0530

    NSE_india sectoral indicies data scrape

--- nse_india/nse_india_database.py
+++ nse_india/nse_india_database.py
@@ -1,19 +1,44 @@
 import pymongo
 import json
 from rich import print
+import jmespath
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["nse_india"]
 mycol = mydb["sectoral_indicies"]
-mycol.delete_many({})   
+mycol.delete_many({})  
+top = mydb["TOP_20_shares"]
+top.delete_many({})  
+bottom = mydb["BOTTOM_20_shares"]
+bottom.delete_many({})   
 # mycol.create_index("order_code",unique=True)
 
-with open (r"C:\Python Training\nse_india\sectoral_indices_stock_data.json","r",encoding= "utf-8")as f:
-    data = json.load(f)
+#high and low 20 shares
+
+with open (r"C:\Python Training\nse_india\sectoral_indices_stock_data.json",'r',encoding='utf-8') as f:
+    new_data=json.load(f)
+
+    all_stocks = jmespath.search("[*].sectoral_data[]", new_data)
+    # print(all_stocks)
+
+    seen = set()
+    unique_stocks = []
+    for stock in all_stocks:
+        if stock["symbol"] not in seen:
+            seen.add(stock["symbol"])
+            unique_stocks.append(stock)
+
+    top_20_gainers = sorted(unique_stocks, key=lambda x: x["change_percentage"] or 0, reverse=True)[:20]
+    bottom_20_losers = sorted(unique_stocks, key=lambda x: x["change_percentage"] or 0, reverse=False)[:20]
+
+    print(top_20_gainers)
+    print(bottom_20_losers)
 
 try:
-    mycol.insert_many(data)
+    mycol.insert_many(new_data)
+    top.insert_many(top_20_gainers)
+    bottom.insert_many(bottom_20_losers)
     print("Data inserted")
 except Exception as e:
     print("Dupliced product skipped")
